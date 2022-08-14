4455ba627bc704b02b75645f66081b14c1b6a9f5
Frederic <bemeraude@hotmail.com>
2022-08-14 16:55:04 -0400

    auto commit

--- Influence/loo.py
+++ Influence/loo.py
@@ -9,6 +9,7 @@ class loo_influence():
 
 	def calc_influence(self, model, train, dev):
 		"""Takes in: a fully trained model,  the training set, and the dev set for which to calculate the influence"""
+		results_full = torch.zeros((len(dev)))
 		data_loader = DataLoader(
 			dataset=dev,
 			batch_size=1,
@@ -18,7 +19,7 @@ class loo_influence():
 		)
 		for j, batch in enumerate(data_loader):
 			with torch.no_grad():
-				loss = classifier.get_loss(batch).item()
+				loss = model.get_loss(batch).item()
 			results_full[j] = loss
 
 		print(results_full)
