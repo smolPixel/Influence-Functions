346fdc54e35e55a2c99e6512316024429ab2cdce
Frederic <bemeraude@hotmail.com>
2022-08-15 16:06:46 -0400

    auto commit

--- Influence/loo.py
+++ Influence/loo.py
@@ -2,6 +2,7 @@
 from torch.utils.data import DataLoader
 import torch
 from utils import set_seed
+from copy import deepcopy
 
 class loo_influence():
 
@@ -23,26 +24,16 @@ class loo_influence():
 				loss = model.get_loss(batch).item()
 			results_full[j] = loss
 
-		print(results_full)
 		influence=torch.zeros((len(train), len(dev)))
 
 
 		#First thing first test the reset of the model and retraining
 		# set_seed(self.argdict['random_seed'])
 		model.reset()
-		model.train()
-		results_full = torch.zeros((len(dev)))
-		data_loader = DataLoader(
-			dataset=dev,
-			batch_size=1,
-			shuffle=False,
-			# num_workers=cpu_count(),
-			pin_memory=torch.cuda.is_available()
-		)
-		for j, batch in enumerate(data_loader):
-			with torch.no_grad():
-				loss = model.get_loss(batch).item()
-			results_full[j] = loss
+		print(len(train))
+		training_new=deepcopy(train)
+		training_new.data.pop(0)
+		training_new.reset_index()
+		print(len(training_new))
+		fds
 
-		influence=torch.zeros((len(train), len(dev)))
-		print(results_full)
