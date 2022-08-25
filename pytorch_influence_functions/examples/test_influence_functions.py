8259c9d839abc338d0e18233c7371464281f57d7
Frederic <bemeraude@hotmail.com>
2022-08-25 15:06:12 -0400

    auto commit

--- pytorch_influence_functions/examples/test_influence_functions.py
+++ pytorch_influence_functions/examples/test_influence_functions.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
 import pytorch_influence_functions.pytorch_influence_functions as ptif
-from train_influence_functions import load_model, load_data
+from pytorch_influence_functions.pytorch_influence_functions.examples.train_influence_functions import load_model, load_data
 
 if __name__ == "__main__":
     config = ptif.utils.get_default_config()
