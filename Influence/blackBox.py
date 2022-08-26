adf0c1ece11c2f0b481e4bb6cde90e3ceffc782f
Frederic <bemeraude@hotmail.com>
2022-08-26 12:44:42 -0400

    auto commit

---
+++ Influence/blackBox.py
@@ -0,0 +1,27 @@
+"""Leave one out for finding the influence"""
+from torch.utils.data import DataLoader
+import torch
+from utils import set_seed
+from copy import deepcopy
+
+class BlackBox_influence():
+	"""Calculate influence of training examples for a dev (or test) example following https://arxiv.org/pdf/1703.04730.pdf"""
+
+	def __init__(self, argdict):
+		self.argdict=argdict
+
+	def calc_influence(self, model, train, dev):
+		"""Takes in: a fully trained model,  the training set, and the dev set for which to calculate the influence"""
+		results_full = torch.zeros((len(dev)))
+		data_loader = DataLoader(
+			dataset=dev,
+			batch_size=1,
+			shuffle=False,
+			# num_workers=cpu_count(),
+			pin_memory=torch.cuda.is_available()
+		)
+
+
+		for datapoint in data_loader:
+			print(datapoint)
+			fds
--- Influence/influence.py
+++ Influence/influence.py
@@ -7,6 +7,9 @@ class influence():
 		if argdict['influence_function'].lower()=="loo":
 			from Influence.loo import loo_influence
 			self.model=loo_influence(argdict)
+		elif argdict['influence_function'].lower()=="blackbox":
+			from Influence.blackBox import BlackBox_influence
+			self.model=BlackBox_influence(argdict)
 		else:
 			raise ValueError("Influence function not found")
 
