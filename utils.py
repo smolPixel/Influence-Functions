93e6c4925697b3bfcc3042bc437a1648789d4edc
Frederic <bemeraude@hotmail.com>
2022-08-26 12:59:01 -0400

    auto commit

--- utils.py
+++ utils.py
@@ -8,6 +8,53 @@ def set_seed(seed=42):
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
 
+def s_test(z_test, t_test, model, z_loader, gpu=-1, damp=0.01, scale=25.0,
+           recursion_depth=5000):
+    """s_test can be precomputed for each test point of interest, and then
+    multiplied with grad_z to get the desired value for each training point.
+    Here, strochastic estimation is used to calculate s_test. s_test is the
+    Inverse Hessian Vector Product.
+
+    Arguments:
+        z_test: torch tensor, test data points, such as test images
+        t_test: torch tensor, contains all test data labels
+        model: torch NN, model used to evaluate the dataset
+        z_loader: torch Dataloader, can load the training dataset
+        gpu: int, GPU id to use if >=0 and -1 means use CPU
+        damp: float, dampening factor
+        scale: float, scaling factor
+        recursion_depth: int, number of iterations aka recursion depth
+            should be enough so that the value stabilises.
+
+    Returns:
+        h_estimate: list of torch tensors, s_test"""
+    v = grad_z(z_test, t_test, model, gpu)
+    h_estimate = v.copy()
+
+    ################################
+    # TODO: Dynamically set the recursion depth so that iterations stops
+    # once h_estimate stabilises
+    ################################
+    for i in range(recursion_depth):
+        # take just one random sample from training dataset
+        # easiest way to just use the DataLoader once, break at the end of loop
+        #########################
+        # TODO: do x, t really have to be chosen RANDOMLY from the train set?
+        #########################
+        for x, t in z_loader:
+            if gpu >= 0:
+                x, t = x.cuda(), t.cuda()
+            y = model(x)
+            loss = calc_loss(y, t)
+            params = [ p for p in model.parameters() if p.requires_grad ]
+            hv = hvp(loss, params, h_estimate)
+            # Recursively caclulate h_estimate
+            h_estimate = [
+                _v + (1 - damp) * _h_e - _hv / scale
+                for _v, _h_e, _hv in zip(v, h_estimate, hv)]
+            break
+        display_progress("Calc. s_test recursions: ", i, recursion_depth)
+    return h_estimate
 
 def calc_s_test_single(model, z_test, t_test, train_loader, gpu=-1,
                        damp=0.01, scale=25, recursion_depth=5000, r=1):
