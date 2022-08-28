c0648805dd04974f6f42ab470861074bd41ab27d
Frederic <bemeraude@hotmail.com>
2022-08-28 14:07:47 -0400

    auto commit

--- Influence/blackBox.py
+++ Influence/blackBox.py
@@ -23,7 +23,7 @@ class BlackBox_influence():
 
 		train_loader= DataLoader(
 			dataset=train,
-			batch_size=1,
+			batch_size=32,
 			shuffle=False,
 			# num_workers=cpu_count(),
 			pin_memory=torch.cuda.is_available()
@@ -33,6 +33,34 @@ class BlackBox_influence():
 		for datapoint in test_loader:
 			x=datapoint['input']
 			y=datapoint['label']
-			influence, harmful, helpful, _=calc_s_test_single(model, x, y, train_loader)
-			print(harmful)
-			print('---')
+			s_test=calc_s_test_single(model, x, y, train_loader)
+			#Now that we have the s_test for the test point, we can calculate the influence of each trainng point on it
+			train_dataset_size = len(train_loader.dataset)
+			influences = []
+			for i in range(train):
+				print(train_loader.dataset[i])
+				fds
+				z = train_loader.collate_fn([z])
+				t = train_loader.collate_fn([t])
+				if time_logging:
+					time_a = datetime.datetime.now()
+				grad_z_vec = grad_z(z, t, model, gpu=gpu)
+				if time_logging:
+					time_b = datetime.datetime.now()
+					time_delta = time_b - time_a
+					logging.info(f"Time for grad_z iter:"
+								 f" {time_delta.total_seconds() * 1000}")
+				tmp_influence = -sum(
+					[
+						####################
+						# TODO: potential bottle neck, takes 17% execution time
+						# torch.sum(k * j).data.cpu().numpy()
+						####################
+						torch.sum(k * j).data
+						for k, j in zip(grad_z_vec, s_test_vec)
+					]) / train_dataset_size
+				influences.append(tmp_influence.cpu())
+				display_progress("Calc. influence function: ", i, train_dataset_size)
+
+			harmful = np.argsort(influences)
+			helpful = harmful[::-1]
