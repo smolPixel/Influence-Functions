80e1eabfe46ca86d2ee20ca31ec9816c43267e05
Frederic <bemeraude@hotmail.com>
2022-08-28 11:20:29 -0400

    auto commit

--- utils.py
+++ utils.py
@@ -9,7 +9,37 @@ def set_seed(seed=42):
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
 
+def hvp(y, w, v):
+    """Multiply the Hessians of y and w by v.
+    Uses a backprop-like approach to compute the product between the Hessian
+    and another vector efficiently, which even works for large Hessians.
+    Example: if: y = 0.5 * w^T A x then hvp(y, w, v) returns and expression
+    which evaluates to the same values as (A + A.t) v.
+    Arguments:
+        y: scalar/tensor, for example the output of the loss function
+        w: list of torch tensors, tensors over which the Hessian
+            should be constructed
+        v: list of torch tensors, same shape as w,
+            will be multiplied with the Hessian
+    Returns:
+        return_grads: list of torch tensors, contains product of Hessian and v.
+    Raises:
+        ValueError: `y` and `w` have a different length."""
+    if len(w) != len(v):
+        raise(ValueError("w and v must have the same length."))
 
+    # First backprop
+    first_grads = grad(y, w, retain_graph=True, create_graph=True)
+
+    # Elementwise products
+    elemwise_products = 0
+    for grad_elem, v_elem in zip(first_grads, v):
+        elemwise_products += torch.sum(grad_elem * v_elem)
+
+    # Second backprop
+    return_grads = grad(elemwise_products, w, create_graph=True)
+
+    return return_grads
 
 def grad_z(training_batch, label, classifier, gpu=-1):
     """Calculates the gradient z. One grad_z should be computed for each
