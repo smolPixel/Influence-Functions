8629d7bfda7e6c015cb8225e0423afe2ca794876
Frederic <bemeraude@hotmail.com>
2022-09-07 10:55:06 -0400

    auto commit

--- Influence/influence.py
+++ Influence/influence.py
@@ -10,6 +10,9 @@ class influence():
 		elif argdict['influence_function'].lower()=="blackbox":
 			from Influence.blackBox import BlackBox_influence
 			self.model=BlackBox_influence(argdict)
+		elif argdict['influence_function'].lower()=="blackbox_attack":
+			from Influence.blackBoxAttack import BlackBox_Attack
+			self.model=BlackBox_Attack(argdict)
 		else:
 			raise ValueError("Influence function not found")
 
